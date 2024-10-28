b55503f873e9290efbf47ca1ce2dc684745fa13f
Manoj Bagale <manojbagale04@gmail.com>
2024-10-28 10:17:18 -0500

    [pt.2]: added files and folder to start working on pt.2

---
+++ Part2/search_tests.py
@@ -0,0 +1,38 @@
+from search import search, article_length, unique_authors, most_recent_article, favorite_author, title_and_author, refine_search, display_result
+from search_tests_helper import get_print, print_basic, print_advanced, print_advanced_option
+from wiki import article_metadata
+from unittest.mock import patch
+from unittest import TestCase, main
+
+class TestSearch(TestCase):
+
+    ##############
+    # UNIT TESTS #
+    ##############
+
+    def test_example_unit_test(self):
+        expected_search_soccer_results = [
+            ['Spain national beach soccer team', 'jack johnson', 1233458894, 1526],
+            ['Will Johnson (soccer)', 'Burna Boy', 1218489712, 3562],
+            ['Steven Cohen (soccer)', 'Mack Johnson', 1237669593, 2117]
+        ]
+        self.assertEqual(search('soccer'), expected_search_soccer_results)
+
+    #####################
+    # INTEGRATION TESTS #
+    #####################
+
+    @patch('builtins.input')
+    def test_example_integration_test(self, input_mock):
+        keyword = 'soccer'
+        advanced_option = 1
+        advanced_response = 3000
+
+        output = get_print(input_mock, [keyword, advanced_option, advanced_response])
+        expected = print_basic() + keyword + '\n' + print_advanced() + str(advanced_option) + '\n' + print_advanced_option(advanced_option) + str(advanced_response) + "\n\nHere are your articles: [['Spain national beach soccer team', 'jack johnson', 1233458894, 1526], ['Steven Cohen (soccer)', 'Mack Johnson', 1237669593, 2117]]\n"
+
+        self.assertEqual(output, expected)
+
+# Write tests above this line. Do not remove.
+if __name__ == "__main__":
+    main()
